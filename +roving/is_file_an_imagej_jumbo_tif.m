d8db341df0a62727e4b6389b9c822b4dd73bdbe7
Adam L. Taylor <taylora@janelia.hhmi.org>
2018-01-25 17:10:20 -0500

    Enable loading of single-frame, conventional tiffs as very short "videos".

--- +roving/is_file_an_imagej_jumbo_tif.m
+++ +roving/is_file_an_imagej_jumbo_tif.m
@@ -8,42 +8,46 @@ function result = is_file_an_imagej_jumbo_tif(file_name)
     end
     if numel(info)==1 ,
         if isequal(info.Format,'tif') ,
-            description = info.ImageDescription ;
-            if length(description)>=6 && isequal(description(1:6),'ImageJ') ,
-                % OK, this seems promising...
-                % Need to parse the description string, get out the "images" field.
-                every_pair_a_string = strsplit(description, '\n') ;
-                every_pair_a_pair = cellfun(@(pair_as_string)(strsplit(pair_as_string,'=')), every_pair_a_string, 'UniformOutput', false) ;
-                names = cellfun(@name_from_pair, every_pair_a_pair, 'UniformOutput', false) ;
-                is_match = cellfun(@(name)(isequal(name,'images')), names) ;                
-                i_images = find(is_match,1) ;
-                if isempty(i_images) ,
-                    result = false ;
-                else
-                    images_pair = every_pair_a_pair{i_images} ;
-                    if length(images_pair)>=2 ,
-                        n_frames_as_string = images_pair{2} ;
-                        n_frames = str2double(n_frames_as_string) ;
-                        if isfinite(n_frames) && isreal(n_frames) && (n_frames>=0) && round(n_frames)==n_frames ,
-                            % n_frames looks like a plausible number of frames
-                            % if n_frames is greater than 1, this is an imagej-style multi-page tif
-                            if n_frames==1 ,
-                                % Only one frame, so this is just a normal TIFF
-                                result = false ;
+            if isfield(info, 'ImageDescription') ,
+                description = info.ImageDescription ;
+                if length(description)>=6 && isequal(description(1:6),'ImageJ') ,
+                    % OK, this seems promising...
+                    % Need to parse the description string, get out the "images" field.
+                    every_pair_a_string = strsplit(description, '\n') ;
+                    every_pair_a_pair = cellfun(@(pair_as_string)(strsplit(pair_as_string,'=')), every_pair_a_string, 'UniformOutput', false) ;
+                    names = cellfun(@name_from_pair, every_pair_a_pair, 'UniformOutput', false) ;
+                    is_match = cellfun(@(name)(isequal(name,'images')), names) ;                
+                    i_images = find(is_match,1) ;
+                    if isempty(i_images) ,
+                        result = false ;
+                    else
+                        images_pair = every_pair_a_pair{i_images} ;
+                        if length(images_pair)>=2 ,
+                            n_frames_as_string = images_pair{2} ;
+                            n_frames = str2double(n_frames_as_string) ;
+                            if isfinite(n_frames) && isreal(n_frames) && (n_frames>=0) && round(n_frames)==n_frames ,
+                                % n_frames looks like a plausible number of frames
+                                % if n_frames is greater than 1, this is an imagej-style multi-page tif
+                                if n_frames==1 ,
+                                    % Only one frame, so this is just a normal TIFF
+                                    result = false ;
+                                else
+                                    % The number of frames in the comment differs from one, so must be an ImageJ "jumbo" TIFF!
+                                    result = true ;
+                                end
                             else
-                                % The number of frames in the comment differs from one, so must be an ImageJ "jumbo" TIFF!
-                                result = true ;
+                                result = false ;
                             end
                         else
                             result = false ;
                         end
-                    else
-                        result = false ;
-                    end
-                end                
+                    end                
+                else
+                    result = false ;
+                end
             else
                 result = false ;
-            end
+            end            
         else
             result = false ;
         end
