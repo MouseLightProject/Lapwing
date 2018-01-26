887c57e85395cc62b43251505175d5ccb6a448f4
Adam L. Taylor <taylora@janelia.hhmi.org>
2018-01-26 10:59:57 -0500

    Updated in-repo modpath.m script.

--- modpath.m
+++ modpath.m
@@ -1,20 +1,22 @@
 function modpath()
-  addpath(desvn(genpath('~/taylor_matlab_toolbox/114')));
-  root_dir_name=fileparts(mfilename('fullpath'));
-  addpath(root_dir_name);
+  repo_dir_name=fileparts(mfilename('fullpath'));
+  toolbox_dir_name=fullfile(repo_dir_name,'tmt_116');
+  addpath(degit(genpath(toolbox_dir_name)));
+  addpath(repo_dir_name);
+  addpath(fullfile(repo_dir_name,'utility'));
 end
 
-function path_no_svn=desvn(path_raw)
-  % eliminate .svn directories from a path string
+function path_no_git=degit(path_raw)
+  % eliminate .git directories from a path string
   path_raw_as_array=split_path(path_raw);
-  path_no_svn_as_array=cell(0,1);
+  path_no_git_as_array=cell(0,1);
   for i=1:length(path_raw_as_array)
-    k=strfind(path_raw_as_array{i},'.svn');
+    k=strfind(path_raw_as_array{i},'.git');
     if isempty(k)
-      path_no_svn_as_array{end+1}=path_raw_as_array{i};  %#ok
+      path_no_git_as_array{end+1}=path_raw_as_array{i};  %#ok
     end
   end
-  path_no_svn=combine_path(path_no_svn_as_array);
+  path_no_git=combine_path(path_no_git_as_array);
 end
 
 function path_as_array=split_path(path)
